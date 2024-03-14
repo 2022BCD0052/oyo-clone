c284279ab5b50c92577ce2c5ef27acfab7abfc02
2022BCD0052 <yksaini0193@gmail.com>
2024-03-15 05:23:11 +0530

    m

--- models/user-models.js
+++ models/user-models.js
@@ -1,25 +1,25 @@
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
 
 
-const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: [true, "Please provide a name"],
-        trim: true,
-      },
-      email: {
-        type: String,
-        trim: true,
-        lowercase: true,
-        required: [true, "Please provide an email"],
-        unique: true,
-      },
-      password: {
-        type: String,
-        trim: true,
-        required: [true, "Please provide a password"],
-      },
+// const userSchema = new mongoose.Schema({
+//     name: {
+//         type: String,
+//         required: [true, "Please provide a name"],
+//         trim: true,
+//       },
+//       email: {
+//         type: String,
+//         trim: true,
+//         lowercase: true,
+//         required: [true, "Please provide an email"],
+//         unique: true,
+//       },
+//       password: {
+//         type: String,
+//         trim: true,
+//         required: [true, "Please provide a password"],
+//       },
     
-},{timestamps:true});
+// },{timestamps:true});
 
-export default mongoose.models?.User || mongoose.model("User", userSchema); 
+// export default mongoose.models?.User || mongoose.model("User", userSchema); 
